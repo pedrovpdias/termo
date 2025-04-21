6c11615f6bbc62aef09d79f0d305b2aeb9afd45a
Pedro Dias <pedrovpdias@gmail.com>
2025-04-21 17:43:44 -0300

    Update Keyboard

--- resources/js/components/Keyboard.vue
+++ resources/js/components/Keyboard.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="grid gap-2 text-center z-20">
-    <div v-for="row in layout" :key="row" class="flex justify-center gap-1">
+  <div class="grid gap-2 text-center z-20 py-8 px-2">
+    <div v-for="row in layout" :key="row" class="flex justify-center gap-1 flex-1">
       <button
         v-for="key in row"
         :key="key"
-        class="px-2 py-1 bg-zinc-700 text-white text-base rounded hover:bg-zinc-500 active:bg-zinc-800 focus:bg-zinc-500 outline-none"
+        class="px-2 py-1 bg-zinc-700 text-white text-base rounded hover:bg-zinc-500 active:bg-zinc-800 focus:bg-zinc-500 outline-none min-w-7 shrink"
         @click="$emit('key', key)"
       >
         {{ key }}
